943d0cad82ecabf7f7e2065da4004216c6582f67
Shuabble <127648888+Shuabble@users.noreply.github.com>
2023-03-12 15:21:54 +0800

    Mean Function

---
+++ mean.py
@@ -0,0 +1,26 @@
+# Joshua Mikael P. Diñoso
+# PreLab - 1 Contribution
+# Programming Problem 1 - Mean Function
+
+# Mean function, length of number list will be determined using python's inbuilt length function,
+# and the sum of all numbers will be determined using python's inbuilt sum function.
+# Function will return calculated mean value.
+def meanCalculation(numbers):
+    listLength = len(numbers)
+    summation = sum(numbers)
+    mean = summation/listLength
+    return mean
+
+
+print('\n')
+# Ask the user for a list of numbers.
+number_string = input("Enter a list of numbers separated by commas: ")
+# Split each number input by a comma.
+number_list = [int(num) for num in number_string.split(',')]
+print('\n')
+# Show the list of numbers that the user has inputted.
+print("Number list: ", number_list)
+# Call the mean function to calculate the mean of the given number list.
+mean = meanCalculation(number_list)
+print("\n")
+print("The calculated mean is: ", mean)  # Show the calculated mean.
